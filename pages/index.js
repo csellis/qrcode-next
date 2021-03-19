ac676d92f46635fcb69f4a10e8350c607870b0cc
Chris Ellis <ellis.cs@gmail.com>
2021-03-19 19:23:14 +0000

    feat: update QR scanner

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,84 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import QrReader from "react-qr-scanner";
+import { useState, useEffect, useRef } from "react";
+
+function Example() {
+  let codeRead = useRef("");
+
+  const [scanning, setScanning] = useState(true);
+  const [qr, setqr] = useState("");
+  function onScan(result) {
+    console.clear();
+    console.log(`Identified: ${result}`);
+    console.log(result);
+    if (result) {
+      setScanning(false);
+      codeRead = result;
+      setqr(result);
+    }
+    // setCodeRead(result);
+  }
+
+  function onError(error) {
+    console.error(error);
+  }
+  console.log(codeRead);
+  if (scanning) {
+    return (
+      <QrReader
+        delay={500}
+        style={{
+          width: 320,
+          height: 240,
+        }}
+        onError={onError}
+        onScan={onScan}
+      />
+    );
+  } else {
+    return <p className='bg-yellow-700 text-3xl'>Text: {JSON.stringify(qr.text)}</p>;
+  }
+}
 
 export default function Home() {
+  const [result, setResult] = useState("No result");
+
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
+
+  function handleScan(data) {
+    setResult({ result: data });
+  }
+
+  function handleError(err) {
+    console.error(err);
+  }
+
+  function IsBrowser() {
+    if (typeof window === "undefined") {
+      return null;
+    }
+    return <Example />;
+  }
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <IsBrowser />
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <a href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app' target='_blank' rel='noopener noreferrer'>
+          Powered by <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
